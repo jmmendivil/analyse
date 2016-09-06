f8935b5475a1bb9538eb0f347babb72f16f56e0d
Juan Mendivil <juan@mendivil.mx>
2016-09-06 17:31:11 -0500

    add multi-compiler option

--- app/pages/upload/page.js
+++ app/pages/upload/page.js
@@ -4,6 +4,7 @@ module.exports = function() {
 	var nextPage = Array.prototype.slice.call(arguments);
 	document.title = "upload";
 	$("body").html(require("./application.jade")());
+	$(".children").hide();
 	$(".modal").modal({show: true});
 	$("#file").change(loadFromFile);
 	$("#example").click(loadFromExample);
@@ -21,13 +22,33 @@ module.exports = function() {
 		var fileReader = new FileReader();
 		fileReader.readAsText(files[0]);
 		fileReader.onload = function() {
-			var data = fileReader.result;
-			app.load(JSON.parse(data));
-			$(".modal").modal("hide");
-			app.loadPage.apply(app, nextPage);
+			var data = JSON.parse(fileReader.result);
+
+			if (!data.assets && !data.modules && data.children.length > 1) {
+				var children = [];
+				data.children.forEach(function(val, i) {
+					var name = val.chunks[0].names[0] || "Build #" + (i+1);
+					children.push("<option>" + name + "</option>");
+				})
+				$(".children")
+					.find("select")
+					.append(children)
+					.on("change", function () {
+						var index = this.selectedIndex - 1;
+						loadStats(data.children[index]);
+					})
+					.end()
+					.fadeIn();
+			} else { loadStats(data); }
 		};
 		fileReader.onerror = function(err) {
 			alert(err);
 		};
+	}
+
+	function loadStats(stats) {
+		app.load(stats);
+		$(".modal").modal("hide");
+		app.loadPage.apply(app, nextPage);
 	}
 };
